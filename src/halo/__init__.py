593b701055a7eed3f97793980621e94309cb8d38
Molau <sirko@molau.de>
2026-07-27 01:34:41 +0200

    v3.3.5 - Bump micro release date

--- src/halo/__init__.py
+++ src/halo/__init__.py
@@ -3,6 +3,6 @@ HALO - Atmospheric Halo Observation Software
 Python translation of the original Borland Pascal software
 """
 
-__version__ = "3.3.4"
-__version_date__ = "2026-07-15"
+__version__ = "3.3.5"
+__version_date__ = "2026-07-27"
 __author__ = "Original: 1992, Python Translation: 2025"
